12eb9356b8b66251bb566bf2f399fc777b132443
Kristoffer Andergrim <andergrim@gmail.com>
2014-05-20 02:23:32 +0200

    Support for multiple super providers!

--- super/super.py
+++ super/super.py
@@ -11,7 +11,7 @@ Super
 =head1 DESCRIPTION
  
     This plugin monitors the usage of words including "super" and
-    checkmarks on aftonbladet.se
+    checkmarks on aftonbladet.se and expressen.se
     
     This plugin requires python and Beautiful Soup
     
@@ -66,7 +66,7 @@ find this plugin on github at https://github.com/andergrim/munin-plugins
 
 =head1 VERSION
 
-    1.0
+    1.1
 
 =head1 CHANGELOG
 
@@ -74,10 +74,14 @@ find this plugin on github at https://github.com/andergrim/munin-plugins
  
     First release
 
+=head2 1.1 - 2014-05-20
+ 
+    Support for multiple providers of super news
+
 =cut
 """
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 import os, sys, urllib, re
 
@@ -85,31 +89,68 @@ hostname = os.getenv("hostname", "")
 category = os.getenv("category", "web")
 plugin_name = list(os.path.split(sys.argv[0]))[1]
 
+providers = [
+              { "short":  "ab",
+                "name":   "Aftonbladet",
+                "colour": "CE181E",
+                "url":    "http://www.aftonbladet.se/",
+                "tags":  [ "h1", "h2", "h3", "span", "p" ],
+                "check": {
+                  "element": "span",
+                  "attr":    "class",
+                  "value":   "abSymbBo"
+                }
+              },
+              { "short":  "ex",
+                "name":   "Expressen",
+                "colour": "0366A0",
+                "url":    "http://www.expressen.se/",
+                "tags":  [ "a", "span", "p" ] ,
+                "check": {
+                  "element": "",
+                  "attr"   : "text",
+                  "value"  : "\xe2\x9c\x93"
+                }
+              }
+           ]
+
+
 def config():
+    global providers
+
     print "multigraph " + plugin_name + "_super"
-    print "graph_title Aftonbladet SUPER usage"
+    print "graph_title SUPER usage"
     print "graph_vtitle Occurrences"
     print "graph_args --base 1000 --lower-limit 0"
     print "graph_category " + category
+
     if hostname:
         print "host_name " + hostname
-    print "super.label Super"
-    print "super.draw AREA"
-    print "super.type GAUGE"
-    print "super.min 0"
+
+    for provider in providers:
+      print provider["short"] + "_super.label " + provider["name"]
+      print provider["short"] + "_super.draw AREASTACK"
+      print provider["short"] + "_super.type GAUGE"
+      print provider["short"] + "_super.colour " + provider["colour"]
+      print provider["short"] + "_super.min 0"
+
     print "\n"
 
     print "multigraph " + plugin_name + "_checkmark"
-    print "graph_title Aftonbladet CHECK usage" 
+    print "graph_title CHECKMARK usage" 
     print "graph_vtitle Occurrences"
     print "graph_args --base 1000 --lower-limit 0"
     print "graph_category " + category 
+
     if hostname:
         print "host_name " + hostname
-        print "check.label Check"
-    print "check.draw AREA"
-    print "check.type GAUGE"
-    print "check.min 0"
+
+    for provider in providers:
+      print provider["short"] + "_check.label " + provider["name"]
+      print provider["short"] + "_check.draw AREASTACK"
+      print provider["short"] + "_check.type GAUGE"
+      print provider["short"] + "_check.colour " + provider["colour"]
+      print provider["short"] + "_check.min 0"
 
     sys.exit(0)
 
@@ -124,11 +165,15 @@ def autoconf():
 
 
 def fetch():
-    from bs4 import BeautifulSoup
+  from bs4 import BeautifulSoup
+
+  num_super = { }
+  num_checkmarks = { }
+
+  for provider in providers:
 
     # Fetch web page
-    url = "http://www.aftonbladet.se/"
-    conn = urllib.urlopen(url)
+    conn = urllib.urlopen(provider["url"])
     html = conn.read()
     conn.close()
 
@@ -136,44 +181,55 @@ def fetch():
     soup = BeautifulSoup(html)
 
     # Count occurrences of "super" among all words
-    tags = soup.find_all([ "h1", "h2", "h3", "span", "p" ])
+    tags = soup.find_all(provider["tags"])
 
-    super_sentences = []
     super_words = []
     for tag in tags:
-        words = re.findall(r'\w+', tag.text, re.U)
-        for word in words:
-            if re.search('(?i)super', word, re.U):
-                super_words.append(word.encode("utf-8"))
-                super_sentences.append(tag.text.encode("utf-8"))
+      words = re.findall(r'\w+', tag.text, re.U)
+      for word in words:
+        if re.search('(?i)super', word, re.U):
+          super_words.append(word.encode("utf-8"))
 
-    num_super = len(super_words)
+    # Remove duplicates and sum up
+    super_words = list(set(super_words))
+    num_super[provider["short"]] = len(super_words)
 
     # Count occurrences of checkmarks
-    checkmarks = soup.find_all("span", "abSymbBo")
-    num_checkmarks = len(checkmarks)
+    if provider["check"]["attr"] == "class":
+      checkmarks = soup.find_all(provider["check"]["element"], provider["check"]["value"])
+    elif provider["check"]["attr"] == "text":
+      checkmarks = soup.find_all(text = provider["check"]["value"])
 
-    print "multigraph " + plugin_name + "_super"
-    if isinstance(num_super, int):
-        print "super.value " + str(num_super)
-    else:
-        print "super.value U"
+    num_checkmarks[provider["short"]] = len(checkmarks)
 
-    print "multigraph " + plugin_name + "_checkmark"
-    if isinstance(num_checkmarks, int):
-        print "check.value " + str(num_checkmarks)
+  # Print results
+  print "multigraph " + plugin_name + "_super"
+
+  for provider in providers:
+    tag = provider["short"]
+    if isinstance(num_super[tag], int):
+      print tag + "_super.value " + str(num_super[tag])
     else:
-        print "check.value U"
+      print tag + "_super.value U"
+
+  print "multigraph " + plugin_name + "_checkmark"
+  
+  for provider in providers:
+    tag = provider["short"]
+    if isinstance(num_checkmarks[tag], int):
+      print tag + "_check.value " + str(num_checkmarks[tag])
+    else:
+      print tag + "_check.value U"
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "config":
-            config()
-        elif sys.argv[1] == "autoconf":
-            autoconf()
-        else:
-            raise ValueError, "Unknown parameter '%s'" % sys.argv[1]
-            sys.exit(1)
+  if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+      config()
+    elif sys.argv[1] == "autoconf":
+      autoconf()
+    else:
+      raise ValueError, "Unknown parameter '%s'" % sys.argv[1]
+      sys.exit(1)
 
-fetch()
-sys.exit(0)
+  fetch()
+  sys.exit(0)
